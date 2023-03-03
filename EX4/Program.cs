f46957443c813822e45055de9fc4071ad0074c14
Evgeny <drjackq@yandex.ru>
2023-03-03 11:16:53 +0300

    Добавили простой пример рекурсии

---
+++ EX4/Program.cs
@@ -0,0 +1,13 @@
+﻿double Factor (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n==1) return 1;
+    else return n * Factor(n-1);
+
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factor(i)}");
+}
+
